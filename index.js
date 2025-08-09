32bbb18fb3421a5d28b270f9f3e96cc0bc8ab91c
Quentin Devauchelle <quentin@devauchelle.eu>
2025-08-08 18:08:06 -0700

    feat: Add Uniques items by Icon

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import generateItems from "./items.js";
+import generateUniques from "./uniques.js";
 import generateAreas from "./areas.js";
 import parseStats from "./utils/statsDescriptionParser.js";
 import generateMapMods from "./mapMods.js";
 import generateWorldAreas from "./worldAreas.js";
 
 await generateItems();
+await generateUniques();
 await generateAreas();
 await parseStats();
 await generateMapMods();
