749d9f9766c26c059a4726400c6023ffe7b19214
Quentin Devauchelle <quentin@devauchelle.eu>
2025-08-15 18:50:26 -0700

    feat: Add events generation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import generateAreas from "./areas.js";
 import parseStats from "./utils/statsDescriptionParser.js";
 import generateMapMods from "./mapMods.js";
 import generateWorldAreas from "./worldAreas.js";
+import generateEvents from "./events.js"
 
 await generateItems();
 await generateUniques();
@@ -11,4 +12,6 @@ await generateAreas();
 await parseStats();
 await generateMapMods();
 await generateWorldAreas();
+await generateEvents();
+
 console.log("Data generation completed successfully.");                                  
